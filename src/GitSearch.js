14126e98d1977c5c2a9e124d757c86afec009968
Chris <clj@ollure.com>
2022-03-14 20:23:58 -0500

    testing api

--- src/GitSearch.js
+++ src/GitSearch.js
@@ -3,24 +3,51 @@ import React, { Component } from "react";
 class GitSearch extends Component {
 	state = {
 		searchValue: '',
+		users: []
 
 	};
 	handleOnChange = event => {
 		this.setState({ searchValue: event.target.value });
 	};
 	handleSearch = () => {
+		this.searchApi(this.state.searchValue)
+	};
 
-	}
+	searchApi = userInput => {
+		var userSearch = "https://api.github.com/search/users";
+		fetch(userSearch)
+			.then(response => {
+				return response.json();
+			})
+		.then(jsonData => {
+			this.setState({ users: jsonData.users });
+			console.log(jsonData);
+		});
+	};
 
 	render() {
 		
 		return (
 			<div>
 
-				<h1>Search GitHub</h1>
+				<h1>GitHub User Search</h1>
 
-				<input name="text" type="text" placeholder="Enter your search terms."  value={this.state.searhValue} />
-				<button onClick={this.handleSearch}>Search</button>
+				<input name="text" type="text" placeholder="User Search."  value={this.state.searhValue} />
+				<button onClick={this.handleSearch}>Find a User</button>
+
+				{this.state.users ? (
+					<div>
+						{this.state.users.map((user, index) => (
+							<div key={index}>
+							<h1>{user.login}</h1>
+							<p>test</p>
+						</div>
+						))}
+
+						</div>
+						) : (
+							<p>Search for a user</p>
+					)}
 
 			</div>
 		);
