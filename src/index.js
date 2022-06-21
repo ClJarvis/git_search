cbabaec34c1fa7d337f1d548b6d5673305077776
Chris <clj@ollure.com>
2022-06-20 23:18:05 -0500

    text change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import TestPaginate from './TestPaginate';
-//import Pagination from "./Pagination";
 import RepoSearch from './RepoSearch'
 //import SearchForm from './SearchForm'
 
