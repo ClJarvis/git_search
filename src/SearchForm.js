3743cd899bcec8e402161e7a4f244855920fbbcc
Chris <clj@ollure.com>
2022-07-11 22:14:48 -0500

    css chnage, user not found msg works

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -18,25 +18,17 @@ form.addEventListener('submit', function(e){
 	.then((data) => {
 
 		if(data.id === undefined){
-	function notFound() {
-      var notFound = document.getElementById("notFound");
-      notFound.innerHTML = "Sorry, user "+ nameEntered + " not found!";
 
-    console.log(data)
-}
-      notFound()
-}
-
-	let div = document.getElementById('searchUsers');
+      let notFound = document.getElementById("notFound");
+      notFound.innerHTML = "Sorry, user "+ nameEntered.charAt(0).toUpperCase() + nameEntered.slice(1) + " not found!";
+      
+	} else {
+	   let notFound = document.getElementById("notFound");
+		notFound.innerHTML =" ";
+	}
 
 
-			document.getElementById("searchUsers").innerHTML =`
-			<p class="avatarpic"><a href="https://www.github.com/${nameEntered}" target="blank"> <img class="avatar" src="${data.avatar_url}" class="avatar"/></a></p>
-			<p><strong>Profile:</strong> <a href="https://www.github.com/${nameEntered}" target="blank">${data.login.charAt(0).toUpperCase() + data.login.slice(1)}</a></p>
-			<p><strong>Public Repos:</strong> ${data.public_repos}</p>
-			<hr>
-		`
-		})
+		}) 
 
 
 
